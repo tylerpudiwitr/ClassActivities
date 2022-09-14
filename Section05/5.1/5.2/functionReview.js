add4b5d54f5343c2a5a6e7833a2393f643ee70ef
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-09-14 18:22:16 -0500

    up to section 5.2

--- Section05/5.1/5.2/functionReview.js
+++ Section05/5.1/5.2/functionReview.js
@@ -20,7 +20,7 @@ function testAverage(...scores) {
   );
   return total / scores.length;
 }
-testAverage(92, 71, 85, 83);
+//testAverage(92, 71, 85, 83);
 // with the following parameters: 92, 71, 85, 83
 
 // 1.b
@@ -33,17 +33,44 @@ testAverage(92, 71, 85, 83);
 //    if argument is less than 60, print "<grade>, Failed" then output true
 function gradeCheck(testAverage) {
   if (total => 80) {
-    console.log("Enjoy your summer!");
+    console.log("${testAverage}, Enjoy your summer!");
     return false;
   }
   if (total => 60) {
-    console.log("Extra studying required.");
+    console.log("${testAverage}, Extra studying required.");
     return true;
   }
   if (total < 60) {
-    console.log("Failed");
+    console.log("${testAverage}, Failed");
     return true;
   }
 }
 // Invoke gradeCheck and pass in testAverage as an argument; testAverage should have the same parameters as before
-gradeCheck(testAverage);
+//gradeCheck(testAverage);
+
+// Declare a function called summerPlans
+//  These are the requirements of the function:
+//    Accepts two parameters: name and outcome (result will be passed as an argument through outcome)
+//    Inside the function, create an object called student
+//    this object will have two properties with keys name and school
+//    set the value of the name key to be the name parameter
+//    set the value of the school key to be the outcome parameter
+//    set up an if conditional that examines if outcome is true/false
+//    if true, print "<student name>'s will be attending summer school."
+//    if false, print "<student name>'s will be going on vacation this summer!"
+// Hint, you will need to utilize both dot notation and template literals to properly set up these conditions and what they print
+function summerPlans(studentName, outcome) {
+  const obj = {
+    name: studentName,
+    school: outcome
+  };
+  if (obj.school == true) {
+    console.log(`${obj.name} will be attending summer school.`);
+  }
+  if(obj.school == false) {
+    console.log(`${obj.name} will be going on vacation this summer!`);
+}
+
+
+// Invoke summerPlans with a name of your choice and the result variable
+summerPlans(gradeCheck(testAverage(92, 71, 85, 83)));
